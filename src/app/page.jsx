1a0e5e2c21f7873eec4f8118c3e76ccd668d053f
Luiz Felipe Fornari <luizfelipefornari@GMAIL.COM>
2024-10-01 10:24:32 -0300

    home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,42 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Menu from "@/componentes/Menu";
+import { getProdutosDB } from "@/componentes/bd/usecases/produtoUseCases";
+import Link from "next/link";
 
-export default function Home() {
+export const revalidate = 60; // revalida a cada 60 segundos
+
+export default async function Home() {
+  const produtos = await getProdutosDB();
   return (
-    <div>
-      <h1>Home</h1>
+    <div style={{ padding: "20px" }}>
+      <div className="row">
+        {produtos.length > 0 &&
+          produtos.map((objeto) => (
+            <div className="col-sm-3" key={objeto.codigo}>
+              <div className="card mb-3 text-center">
+                <div className="card-header">{objeto.nome}</div>
+                <div className="card-body ">
+                  <h5 className="card-title">{objeto.nome}</h5>
+                  <p className="card-text">
+                    <small className="text-muted">Preço: {objeto.valor}</small>
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Categoria: {objeto.categoria_nome}
+                    </small>
+                  </p>
+                </div>
+                <div class="card-footer text-muted">
+                  <Link
+                    type="button"
+                    className="btn btn-secondary"
+                    href={`/${objeto.codigo}/detalhe`}
+                  >
+                    Detalhes do produto
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
